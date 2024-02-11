c7831890f5330f57e7e151f7d2567c128004d986
Aditya Dave <aditya.dave@smallcase.com>
2024-02-11 16:38:01 +0530

    feat:initial ui

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "jazzteam.org",
+      },
+      {
+        protocol: "https",
+        hostname: "miro.medium.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "qubika.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.analyticsvidhya.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.indianexpress.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.arstechnica.net",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
